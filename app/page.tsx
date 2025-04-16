4715e5e64ce6f87d3887ebe45d6ffe7fbe0be9c3
Jorge Hara Devs <88298206+jorgehara@users.noreply.github.com>
2025-04-15 22:12:07 -0300

    mas columnas

--- app/page.tsx
+++ app/page.tsx
@@ -124,13 +124,13 @@ const Home: React.FC = () => {
         </div>
       )}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
         {mostrarQR && carnetsFiltrados.map((item, index) => (
           <div
-            key={index}
-            className={`flex flex-col items-center p-4 border rounded-lg ${
-              item.marcado ? "bg-red-500" : "bg-black"
-            }`}
+        key={index}
+        className={`flex flex-col items-center p-4 border rounded-lg ${
+          item.marcado ? "bg-red-500" : "bg-black"
+        }`}
           >
             <p className="text-white mb-2 font-mono text-2xl">{item.carnet}</p>
             <QRCode
